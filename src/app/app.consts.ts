b662c58320ae972e62438dd4aeb2182b2d7904ad
Lukas-max <lukaszjan88@gmail.com>
2021-02-19 20:13:34 +0100

    - added more low resolution header images and breakpoints for them - refactored a code in products.component.ts

--- src/app/app.consts.ts
+++ src/app/app.consts.ts
@@ -1,23 +1,23 @@
 // HOST:
-// export const API_URL = 'http://localhost:8080';
+export const API_URL = 'http://localhost:8080';
 // export const API_URL = 'http://51.83.135.196:8080';
-export const API_URL = 'http://localhost:8012';
+// export const API_URL = 'http://localhost:8012';
 
 // // API URL's:
-// export const PRODUCTS_URL = 'api/products';
-export const PRODUCTS_URL = 'auctioshop-products/api/products';
+export const PRODUCTS_URL = 'api/products';
+// export const PRODUCTS_URL = 'auctioshop-products/api/products';
 
-// export const PRODUCT_CATEGORY_URL = 'api/product_category';
-export const PRODUCT_CATEGORY_URL = 'auctioshop-products/api/product_category';
+export const PRODUCT_CATEGORY_URL = 'api/product_category';
+// export const PRODUCT_CATEGORY_URL = 'auctioshop-products/api/product_category';
 
-// export const ORDER_URL = 'api/order';
-export const ORDER_URL = 'auctioshop-ordersusers/api/order';
+export const ORDER_URL = 'api/order';
+// export const ORDER_URL = 'auctioshop-ordersusers/api/order';
 
-// export const USERS_URL = 'api/users';
-export const USERS_URL = 'auctioshop-ordersusers/api/users';
+export const USERS_URL = 'api/users';
+// export const USERS_URL = 'auctioshop-ordersusers/api/users';
 
-// export const USER_LOGIN_URL = 'user';
-export const USER_LOGIN_URL = 'auctioshop-ordersusers/api/login';
+export const USER_LOGIN_URL = 'user';
+// export const USER_LOGIN_URL = 'auctioshop-ordersusers/api/login';
 
 // security and login:
 export const JWT_AUTH_USER = 'jwtAuthUser';
